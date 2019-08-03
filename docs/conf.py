7524fe4bdbca1348e0a18a1ddda09574c65e3605
tubi <esteban.carrillo@sansano.usm.cl>
2019-08-03 02:17:57 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,8 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['source/_static']
 
+master_doc = 'index'
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
