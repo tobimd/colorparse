a97be4eaa06aba4be4047d6e74b2a941fd6158c3
tubi <esteban.carrillo@sansano.usm.cl>
2019-08-03 02:19:16 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'tubi-carrillo'
 release = '0.0.3'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['source/_templates']
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -45,7 +45,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['source/_static']
+html_static_path = ['_static']
 
 master_doc = 'index'
 
